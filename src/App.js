a48ec98bef76bf38223e83f68aa0d8e491a6dd67
hiteshy44 <kairainfotech@gmail.com>
2022-08-24 14:35:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://kairainfotech.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Let's Start
         </a>
       </header>
     </div>
